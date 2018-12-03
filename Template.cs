7b538df1637a72df9fc159c1a38d661203887bf7
Jason Rebelo <igotinfected@gmail.com>
2018-12-03 22:59:51 +0100

    Add template file :sunglasses:

---
+++ Template.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace dayXX
+{
+    class Program
+    {
+        static int PartOne()
+        {
+            return 0;
+        }
+
+        static int PartTwo()
+        {
+            return 0;
+        }
+
+        static IEnumerable<String> Input(StreamReader input)
+        {
+            string line;
+            while ((line = input.ReadLine()) != null)
+                yield return line;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("result 1: " + PartOne());
+            Console.WriteLine("result 2: " + PartTwo());
+        }
+    }
+}
+
